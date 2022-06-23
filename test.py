9a4ebc98b082f15e44d940227d38112118ed8129
Rishiraj Acharya <44090649+rishiraj@users.noreply.github.com>
2022-06-23 22:45:56 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
-df = pd.read_csv("../input/30days-folds/train_folds.csv")
-df_test = pd.read_csv("../input/30-days-of-ml/test.csv")
-sample_submission = pd.read_csv("../input/30-days-of-ml/sample_submission.csv")
+df = pd.read_csv("../input/tpsfeb21-folds/train_folds.csv")
+df_test = pd.read_csv("../input/tabular-playground-series-feb-2021/test.csv")
+sample_submission = pd.read_csv("../input/tabular-playground-series-feb-2021/sample_submission.csv")
 
-useful_features = [c for c in df.columns if c not in ("id", "target", "kfold")]
+useful_features = [c for c in df.columns if c not in ("id", "kfold")]
 object_cols = [col for col in useful_features if 'cat' in col]
-df_test = df_test[useful_features]
+useful_features_test = [c for c in df.columns if c not in ("id", "target", "kfold")]
+df_test = df_test[useful_features_test]
 
 final_test_predictions = []
 final_valid_predictions = {}
 scores = []
 for fold in range(5):
-    xtrain =  df[df.kfold != fold].reset_index(drop=True)
+    xtrain = df[df.kfold != fold].reset_index(drop=True)
     xvalid = df[df.kfold == fold].reset_index(drop=True)
     xtest = df_test.copy()
     
@@ -27,25 +28,16 @@ for fold in range(5):
     xvalid[object_cols] = ordinal_encoder.transform(xvalid[object_cols])
     xtest[object_cols] = ordinal_encoder.transform(xtest[object_cols])
     
-    params = {
-        'random_state': 1, 
-        'booster': 'gbtree',
-        'n_estimators': 10000,
-        'learning_rate': 0.03628302216953097,
-        'reg_lambda': 0.0008746338866473539,
-        'reg_alpha': 23.13181079976304,
-        'subsample': 0.7875490025178415,
-        'colsample_bytree': 0.11807135201147481,
-        'max_depth': 3
-    }
+    xtrain_ds = tfdf.keras.pd_dataframe_to_tf_dataset(xtrain, label="target", task=tfdf.keras.Task.REGRESSION)
+    xvalid_ds = tfdf.keras.pd_dataframe_to_tf_dataset(xvalid, label="target", task=tfdf.keras.Task.REGRESSION)
+    xtest_ds = tfdf.keras.pd_dataframe_to_tf_dataset(xtest, task=tfdf.keras.Task.REGRESSION)
     
-    model = XGBRegressor(
-        n_jobs=4,
-        **params
-    )
-    model.fit(xtrain, ytrain, early_stopping_rounds=300, eval_set=[(xvalid, yvalid)], verbose=1000)
-    preds_valid = model.predict(xvalid)
-    test_preds = model.predict(xtest)
+    model = tfdf.keras.GradientBoostedTreesModel(hyperparameter_template="better_default", task=tfdf.keras.Task.REGRESSION)
+    model.compile(metrics=[tf.keras.metrics.RootMeanSquaredError()])
+    model.fit(x=xtrain_ds)
+    
+    preds_valid = model.predict(xvalid_ds)
+    test_preds = model.predict(xtest_ds)
     final_test_predictions.append(test_preds)
     final_valid_predictions.update(dict(zip(valid_ids, preds_valid)))
     rmse = mean_squared_error(yvalid, preds_valid, squared=False)
@@ -63,19 +55,20 @@ sample_submission.to_csv("test_pred_1.csv", index=False)
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-df = pd.read_csv("../input/30days-folds/train_folds.csv")
-df_test = pd.read_csv("../input/30-days-of-ml/test.csv")
-sample_submission = pd.read_csv("../input/30-days-of-ml/sample_submission.csv")
+df = pd.read_csv("../input/tpsfeb21-folds/train_folds.csv")
+df_test = pd.read_csv("../input/tabular-playground-series-feb-2021/test.csv")
+sample_submission = pd.read_csv("../input/tabular-playground-series-feb-2021/sample_submission.csv")
 
-useful_features = [c for c in df.columns if c not in ("id", "target", "kfold")]
+useful_features = [c for c in df.columns if c not in ("id", "kfold")]
 object_cols = [col for col in useful_features if 'cat' in col]
-df_test = df_test[useful_features]
+useful_features_test = [c for c in df.columns if c not in ("id", "target", "kfold")]
+df_test = df_test[useful_features_test]
 
 final_test_predictions = []
 final_valid_predictions = {}
 scores = []
 for fold in range(5):
-    xtrain =  df[df.kfold != fold].reset_index(drop=True)
+    xtrain = df[df.kfold != fold].reset_index(drop=True)
     xvalid = df[df.kfold == fold].reset_index(drop=True)
     xtest = df_test.copy()
     
@@ -92,25 +85,16 @@ for fold in range(5):
     xvalid[object_cols] = ordinal_encoder.transform(xvalid[object_cols])
     xtest[object_cols] = ordinal_encoder.transform(xtest[object_cols])
     
-    params = {
-        'learning_rate': 0.07853392035787837,
-        'reg_lambda': 1.7549293092194938e-05,
-        'reg_alpha': 14.68267919457715, 
-        'subsample': 0.8031450486786944, 
-        'colsample_bytree': 0.170759104940733, 
-        'max_depth': 3
-    }
+    xtrain_ds = tfdf.keras.pd_dataframe_to_tf_dataset(xtrain, label="target", task=tfdf.keras.Task.REGRESSION)
+    xvalid_ds = tfdf.keras.pd_dataframe_to_tf_dataset(xvalid, label="target", task=tfdf.keras.Task.REGRESSION)
+    xtest_ds = tfdf.keras.pd_dataframe_to_tf_dataset(xtest, task=tfdf.keras.Task.REGRESSION)
     
-    model = XGBRegressor(
-        random_state=fold,
-        n_jobs=4,
-        n_estimators=5000,
-        **params
-    )
-    model.fit(xtrain, ytrain, early_stopping_rounds=300, eval_set=[(xvalid, yvalid)], verbose=1000)
+    model = tfdf.keras.GradientBoostedTreesModel(hyperparameter_template="benchmark_rank1", task=tfdf.keras.Task.REGRESSION)
+    model.compile(metrics=[tf.keras.metrics.RootMeanSquaredError()])
+    model.fit(x=xtrain_ds)
     
-    preds_valid = model.predict(xvalid)
-    test_preds = model.predict(xtest)
+    preds_valid = model.predict(xvalid_ds)
+    test_preds = model.predict(xtest_ds)
     final_test_predictions.append(test_preds)
     final_valid_predictions.update(dict(zip(valid_ids, preds_valid)))
     rmse = mean_squared_error(yvalid, preds_valid, squared=False)
